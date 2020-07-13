e5374285f11a3269fd87f7e5ac32b9246f836858
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-07-13 14:54:55 +0530

    cloak-easy

--- invisible_cloak.py
+++ invisible_cloak.py
@@ -10,28 +10,25 @@ while cap.isOpened():
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         l_red = np.array([0, 120, 150])
         u_red = np.array([10, 255, 255])
-        mask1 = cv2.inRange(hsv, l_red, u_red)
 
-        l_red = np.array([170, 120, 70])
-        u_red = np.array([180, 255, 255])
-        mask2 = cv2.inRange(hsv, l_red, u_red)
+        mask = cv2.inRange(hsv, l_red, u_red)
+        # cv2.imshow("mask", mask) # only red
 
-        mask = mask1 + mask2
-        cv2.imshow("mask", mask)
+        # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
+        part1 = cv2.bitwise_and(back, back, mask=mask)
+        # cv2.imshow("mask", part1) # behind only the red one, else everything black
 
-        # mask1 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3, 3), np.uint8))
-        mask1 = cv2.morphologyEx(mask1, cv2.MORPH_DILATE, np.ones((3, 3), np.uint8))
-        part1 = cv2.bitwise_and(back, back, mask=mask1)
+        mask = cv2.bitwise_not(mask) # all things other than the red
+        # cv2.imshow("mask", mask)
 
-        mask2 = cv2.bitwise_not(mask1)
-        part2 = cv2.bitwise_and(frame, frame, mask=mask2)
-        invisible = cv2.addWeighted(part1, 1, part2, 1, 1)
-        cv2.imshow("magic", invisible)
+        part2 = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow("mask", part2) # real time feed, only red will turn black
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.imshow("mask", part2 + part1)
+
+        if cv2.waitKey(5) == ord('q'):
             break
-    else:
-        break
 
 cap.release()
 cv2.destroyAllWindows()
