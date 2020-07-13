7a741dbc4147914fc8ee6e1307153d87638c2aec
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2020-07-13 14:55:21 +0530

    cloak-easy

--- backgroundimg.py
+++ backgroundimg.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture(0)
 
 while cap.isOpened():
     ret, back = cap.read()
-    if ret==True:
+    if ret:
         cv2.imshow("image.jpg", back)
         if cv2.waitKey(5) == ord('q'):
             cv2.imwrite('image.jpg', back)
